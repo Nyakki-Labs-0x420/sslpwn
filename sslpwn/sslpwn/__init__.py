24e4e98058025124f373e4fc339841a1a07a4643
Nyakki <Nyakki-Labs-0x420@proton.me>
2026-06-24 09:06:30 -0400

    Update __init__.py

--- sslpwn/sslpwn/__init__.py
+++ sslpwn/sslpwn/__init__.py
@@ -1,7 +1,7 @@
-"""sslpwn – Automated testing of BEAST, Lucky13, BREACH, POODLE, CRIME, Heartbleed, Ticketbleed, and ROBOT vulnerabilities."""
+"""sslpwn; Automated testing of BEAST, Lucky13, BREACH, POODLE, CRIME, Heartbleed, Ticketbleed, Renegotiation, Freak, Logjam and ROBOT vulnerabilities."""
 import urllib3
 
-__version__ = "1.0.0"
+__version__ = "2.0.0"
 
 # Suppress insecure HTTPS request warnings globally
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
